fcfc222a00c7ba0a7ba088f8a6399228195ba6d3
Narendra Kumar <30648034+nar3n12@users.noreply.github.com>
2020-11-23 11:46:11 +0530

    Initial commit

---
+++ Assets/Scripts[m]/ClickHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Collider))]
+public class ClickHandler : MonoBehaviour
+{
+    public UnityEvent downEvent;
+    public UnityEvent upEvent;
+    private Animator _anim;
+   // private int _flag = 0;
+
+    private void Start()
+    {
+        _anim = gameObject.GetComponent<Animator>();
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("clicked");
+            _anim.Play("onclick");
+            downEvent?.Invoke();
+      
+       /** else if (_flag == 1)
+        {
+            _flag = 0;
+            _anim.Play("blink");
+            UpEvent?.Invoke();
+        } **/
+    }
+
+    private void OnMouseUp()
+    {
+        upEvent?.Invoke();
+        Debug.Log("up event invoked");
+    }
+}
---
+++ Assets/Scripts[m]/VoiceController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TextSpeech;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Android;
+
+public class VoiceController : MonoBehaviour
+{
+    private const string LANG_CODE = "en-US";
+
+    [SerializeField] Text uiText;
+    [SerializeField] GameObject rin;
+    private Animator animation;
+    private void Start()
+    {
+        Setup(LANG_CODE);
+        //_rin = this.gameObject;
+        //Debug.Log("gameobject: "+ rin.name);
+        animation = rin.GetComponent<Animator>();
+        Debug.Log("voice controller ");
+#if UNITY_ANDROID
+        SpeechToText.instance.onPartialResultsCallback = OnPartialSpeechResult;
+#endif
+        
+        SpeechToText.instance.onResultCallback = OnFinalSpeechResult;
+        CheckPermission();
+        
+    }
+
+    void CheckPermission()
+    {
+#if UNITY_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Permission.RequestUserPermission(Permission.Microphone);
+        }
+#endif
+       
+    }
+
+    #region Speech to Text
+   
+    public void StartListening()
+    {
+        SpeechToText.instance.StartRecording();
+        Debug.Log("recording");
+
+    }
+    public void StopListening()
+    {
+        SpeechToText.instance.StopRecording();
+        Debug.Log("stopped recording");
+    }
+
+    void OnFinalSpeechResult(string result)
+    {
+        Debug.Log("done: "+result);
+        uiText.text = result;
+        if (result == "please walk" || result == "walk")
+        {
+            animation.Play("Walk");
+        }
+        else if (result == "please run" || result == "run")
+        {
+            animation.Play("Run");
+        }
+        else if (result == "please stop" || result == "stop")
+        {
+            animation.Play("Idle");
+        }
+        else if (result == "hello" || result == "good morning")
+        {
+            animation.Play("Bow");
+        }
+    }
+    
+    void OnPartialSpeechResult(string result)
+    {
+        uiText.text = result;
+    }
+
+    #endregion
+
+    void Setup(string code)
+    {
+        SpeechToText.instance.Setting(code);
+    }
+}
